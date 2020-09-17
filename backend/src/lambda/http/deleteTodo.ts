7ecb6c3ab378cc998a4799a5463e517bc020ae74
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-09-17 08:22:38 +0200

    Handle delete todo method and update the serverless with the right permission

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,67 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+import { TodoItem } from '../../models/TodoItem'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
-//   // TODO: Remove a TODO item by id
-//   return undefined
-// }
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+
+    // TODO: Remove a TODO item by id
+    const userId = "1"
+
+
+    const result = await docClient.get({
+        TableName: todosTable,
+        Key: {
+            todoId
+        }
+    }).promise()
+
+    const todo_item = result.Item as TodoItem
+
+    // Todo item is not found
+    if (!todo_item) {
+        return {
+            statusCode: 404,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: ''
+        }
+    }
+
+    // User is not allowed to delete the todo
+    if (todo_item.userId !== userId) {
+        return {
+            statusCode: 403,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: ''
+        }
+    }
+    // Delete Item from DB
+    await deleteTodoItemFromDB(todo_item.todoId)
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify(todo_item.todoId)
+    }
+}
+
+
+async function deleteTodoItemFromDB(todoId: String) {
+    await docClient.delete({
+        TableName: todosTable,
+        Key: {
+            todoId
+        }
+    }).promise()
+}
